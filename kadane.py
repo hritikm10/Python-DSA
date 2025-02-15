7c03583487471befb560b940e81e198656228b90
Hritik Middha <Hritikmiddha10@gmail.com>
2025-02-15 19:22:07 +0530

    Adding Kadane - Finding Maximum Sum of a Sub Array

---
+++ kadane.py
@@ -0,0 +1,9 @@
+def maxSubArraySum(self, arr):
+    maximum = arr[0]
+    recentmax = arr[0]
+    for i in range(1, len(arr)):
+        recentmax = max(recentmax + arr[i], arr[i])
+        maximum = max(recentmax, maximum)
+    return maximum
+
+#
