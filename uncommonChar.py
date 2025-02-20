7518a7ccb2ef6fd987aea63d0bb55cd3b973ace4
Hritik Middha <Hritikmiddha10@gmail.com>
2025-02-20 23:38:53 +0530

    Code uploaded

---
+++ uncommonChar.py
@@ -0,0 +1,11 @@
+def uncommonChars(self, s1, s2):
+    set1 = set(s1)
+    set2 = set(s2)
+    unique_chars = set1.symmetric_difference(set2)
+    result = ''.join(sorted(unique_chars))
+    return resultdef uncommonChars(self, s1, s2):
+        set1 = set(s1)
+        set2 = set(s2)
+        unique_chars = set1.symmetric_difference(set2)
+        result = ''.join(sorted(unique_chars))
+        return result
