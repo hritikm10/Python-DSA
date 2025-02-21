d463ccd74984c073d138ba3e9258e8e3f6346849
Hritik Middha <Hritikmiddha10@gmail.com>
2025-02-21 15:09:26 +0530

    Implement Stack using srray

---
+++ implementStackUsingArray.py
@@ -0,0 +1,7 @@
+def push(self, data):
+    self.arr.append(data)
+
+def pop(self):
+    if self.arr:
+        return self.arr.pop()
+    return -1
--- uncommonChar.py
+++ uncommonChar.py
@@ -3,9 +3,4 @@ def uncommonChars(self, s1, s2):
     set2 = set(s2)
     unique_chars = set1.symmetric_difference(set2)
     result = ''.join(sorted(unique_chars))
-    return resultdef uncommonChars(self, s1, s2):
-        set1 = set(s1)
-        set2 = set(s2)
-        unique_chars = set1.symmetric_difference(set2)
-        result = ''.join(sorted(unique_chars))
-        return result
+    return result
